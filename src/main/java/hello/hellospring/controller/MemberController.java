bd02232ab57cc1193dd0925c4ff8d405b8529a14
ro117youshinKIM <livebyfaith117@naver.com>
2022-07-09 15:30:47 +0900

    자바 코드로 직접 스프링 빈 등록하기 : MemberService와 MemberRepository의 @Service, @Repository, @Autowired 애노테이션을 제거하고 진행

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,16 +1,28 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+
+//    DI : 필드 주입
+//    @Autowired private MemberService memberService;
+
+//    DI : setter 주입
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
     private final MemberService memberService;
 
+//    DI : 생성자 주입 (사용 권장)
+//    생성자에 @Autowired가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다.
+//    DI(Dependency Injection), 의존성 주입
     @Autowired
-    // 생성자에 @Autowired가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다.
-    // DI(Dependency Injection), 의존성 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
